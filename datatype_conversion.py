cf5749ef8bfa97839aafb6c283989dc5424f939f
chinmoy-kumar <chinmoy.coder@gmail.com>
2025-10-20 00:08:34 +0600

    created datatype_conversion.py file

---
+++ datatype_conversion.py
@@ -0,0 +1,23 @@
+# integer to string conversion
+n = 123
+
+result = str(n)
+
+print(type(result))
+print(result)
+
+# string to integer conversion
+s = "123"
+
+s_int = int(s)
+
+print(type(s_int))
+print(s)
+
+# integer to float conversion
+a = 123
+
+a_flaot = float(a)
+
+print(type(a_flaot))
+print(a_flaot)
