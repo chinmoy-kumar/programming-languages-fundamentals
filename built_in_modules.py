85964de15ebf145e9103a50e77b29625021f79c0
chinmoy-kumar <chinmoy.coder@gmail.com>
2026-01-23 01:14:55 +0600

    add built_in_modules.py

---
+++ built_in_modules.py
@@ -0,0 +1,15 @@
+# Ref. -> https://docs.python.org/3/py-modindex.html
+# Modules are the collection of related functions
+# For using modules import them at first
+from math import *
+from random import *
+from time import sleep
+
+result = ceil(3.0001)
+print(result)
+result2 = floor(3.9999)
+print(result2)
+print(random())
+print(randint(1, 100)) # prints a random integer number in that range
+sleep(3) # it will wait or sleep for 3 seconds before proceed
+print(choice(['A', 'B', 'C', 'D', 'E']))
