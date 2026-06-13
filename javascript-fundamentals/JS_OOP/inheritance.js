530c8241faa2a8fef50b652b038db7bb0a2f4028
chinmoy-kumar <chinmoy.coder@gmail.com>
2026-06-14 00:44:37 +0600

    add instance checking in inheritance.js

--- javascript-fundamentals/JS_OOP/inheritance.js
+++ javascript-fundamentals/JS_OOP/inheritance.js
@@ -9,7 +9,15 @@ class Vehicle{
     {
         return `${this.model} is from ${this.brand}`;
     }
+
+    // check if an object is instance of a class or not
+    static isVehicle(obj)
+    {
+        return obj instanceof Vehicle;
+    }
 }
+
+
 
 // inheritance -> using extends keywords
 class Car extends Vehicle{
@@ -23,11 +31,28 @@ class Car extends Vehicle{
     {
         return `${this.model} is a good model of car`;
     }
+
+    static isCar(obj)
+    {
+        return obj instanceof Car;
+    }
 }
 
 // create object
-let car1 = new Car("Corolla", "Toyota");
+let car1 = new Car("Corolla", "Toyota", "ABC-123");
+let vehicle1 = new Vehicle("SUVs", "BMW");
 
 console.log(car1.vechileInfo()); // inherits from Vehicle class
 console.log(car1.drive());
 console.log(car1.model);
+
+let res = Vehicle.isVehicle(car1);
+console.log(res); // true
+res = Car.isCar(car1);
+console.log(res); // true
+
+res = Car.isCar(vehicle1);
+console.log(res); // false
+res = Vehicle.isVehicle(vehicle1);
+console.log(res); // true
+
