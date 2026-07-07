f0512f0b29199b9c298798b7dbedf3d2e2c67d0e
chinmoy-kumar <chinmoy.coder@gmail.com>
2026-07-07 18:57:34 +0600

    add asyncAwait.js

---
+++ javascript-fundamentals/Adv_JS/asyncAwait.js
@@ -0,0 +1,28 @@
+
+
+function fetchUserData()
+{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject({name: "chinmoy", url: "www.chinmoykumar.com"});
+        }, 3000);
+    });
+}
+
+async function getUserData()
+{
+    try{
+        console.log(`Fetching data...`);
+        // 'await' can only be used where there is the `async` keyword over the function.
+        const userData = await fetchUserData();
+        console.log("User data fetched successfully");
+        console.log("User data: ", userData)
+        
+    }
+    catch(error){
+        console.error("Error fetching data", error);
+    }
+}
+
+getUserData();
+
