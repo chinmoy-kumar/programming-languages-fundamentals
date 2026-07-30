532b78cff3100d6470031815e34f7d6130dae2a9
chinmoy-kumar <chinmoy.coder@gmail.com>
2026-07-30 20:39:03 +0600

    modify generatorIterator.js

--- javascript-fundamentals/Adv_JS/generatorIterator.js
+++ javascript-fundamentals/Adv_JS/generatorIterator.js
@@ -1,13 +1,13 @@
 // Rarely used
 
-function* numberGeneraotor()
+function* numberGenerator() // * after function indicates that it is a generator function
 {
     yield 1;
     yield 2; 
     yield 3;
 }
 
-let gen = numberGeneraotor();
+let gen = numberGeneraotor(); // it doesn't print anything, it's just setup the generator function
 let genTwo = numberGeneraotor();
 
 console.log(gen.next().value);
@@ -17,3 +17,15 @@ console.log(gen.next().value); // ouput: undefined bcause only 3 yield are there
 
 console.log(genTwo.next().value);
 console.log(genTwo.next().value);
+
+// NOTE:
+// yield is like a "pause" button. Each time the generator hits a yield, it:
+// - Produces that value
+// - Pauses execution right there
+// - Waits until something asks for the next value
+// This is different from return, which exits the function completely.
+
+// gen.next() -> returns two property `done` and `value`.
+// done: false means more codes to run
+// done: true means no more code to run
+// value means whatever is yielded
